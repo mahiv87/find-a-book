1a04482cf3826030692552623dd6f19dac8d242e
Marcus mbp <maherreraiv1987@gmail.com>
2022-07-17 18:44:09 -0600

    queries added to resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,22 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+	Query: {
+		users: async () => {
+			return User.find().populate('savedBooks');
+		},
+		user: async (parent, { username }) => {
+			return User.findOne({ username }).populate('savedBooks');
+		},
+		me: async (parent, args, context) => {
+			if (context.user) {
+				return User.findOne({ _id: context.user._id }).populate(
+					'savedBooks'
+				);
+			}
+			throw new AuthenticationError('You need to be logged in!');
+		}
+	}
+};
