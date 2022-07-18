2f7ed0bc6f341b1aa9043b06bcd13ec5fbaf97c9
Marcus Herrera <maherreraiv1987@gmail.com>
2022-07-18 07:31:26 -0600

    saveBook and deleteBook mutations added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,9 +7,11 @@ const resolvers = {
 		users: async () => {
 			return User.find().populate('savedBooks');
 		},
+
 		user: async (parent, { username }) => {
 			return User.findOne({ username }).populate('savedBooks');
 		},
+
 		me: async (parent, args, context) => {
 			if (context.user) {
 				return User.findOne({ _id: context.user._id }).populate(
@@ -18,10 +20,12 @@ const resolvers = {
 			}
 			throw new AuthenticationError('You need to be logged in!');
 		},
+
 		books: async (parent, { username }) => {
 			const params = username ? { username } : {};
 			return Book.find(params).sort({ createdAt: -1 });
 		},
+
 		book: async (parent, { bookId }) => {
 			return Book.findOne({ _id: bookId });
 		}
@@ -33,6 +37,7 @@ const resolvers = {
 			const token = signToken(user);
 			return { token, user };
 		},
+
 		login: async (parent, { email, password }) => {
 			const user = await User.findOne({ email });
 
@@ -51,6 +56,39 @@ const resolvers = {
 			const token = signToken(user);
 
 			return { token, user };
+		},
+
+		saveBook: async (parent, { input }, context) => {
+			if (context.user) {
+				return User.findOneAndUpdate(
+					{ _id: context.user._id },
+					{
+						$addToSet: { saveBooks: { input } }
+					},
+					{
+						new: true,
+						runValidators: true
+					}
+				);
+			}
+
+			throw new AuthenticationError('You need to be logged in!');
+		},
+
+		deleteBook: async (parent, { bookId }, context) => {
+			if (context.user) {
+				return User.findOneAndUpdate(
+					{ _id: context.user._id },
+					{
+						$pull: { savedBooks: { bookId } }
+					},
+					{ new: true }
+				);
+			}
+
+			throw new AuthenticationError('You need to be logged in!');
 		}
 	}
 };
+
+module.exports = resolvers;
