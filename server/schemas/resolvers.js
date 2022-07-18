7eee21b2f1eb7d4681f3b8d2fecc916b2b4656b8
Marcus mbp <maherreraiv1987@gmail.com>
2022-07-17 18:52:51 -0600

    user mutations added to resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -25,5 +25,32 @@ const resolvers = {
 		book: async (parent, { bookId }) => {
 			return Book.findOne({ _id: bookId });
 		}
+	},
+
+	Mutation: {
+		addUser: async (parent, { username, email, password }) => {
+			const user = await User.create({ username, email, password });
+			const token = signToken(user);
+			return { token, user };
+		},
+		login: async (parent, { email, password }) => {
+			const user = await User.findOne({ email });
+
+			if (!user) {
+				throw new AuthenticationError(
+					'No user found with this email address'
+				);
+			}
+
+			const correctPw = await user.isCorrectPassword(password);
+
+			if (!correctPw) {
+				throw new AuthenticationError('Incorrect credentials');
+			}
+
+			const token = signToken(user);
+
+			return { token, user };
+		}
 	}
 };
